53553b693d61b896642297f5ef33c2d0d58ce1f9
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-09-18 00:31:15 +0500

    feat: create ExpressionTree.cpp and ExpressionTree.h (Expression Tree Class created)

---
+++ Tree/ExpressionTree/ExpressionTree.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstring>
+#include "ExpressionTree.h"
+
+using namespace std;
+
+bool isOperator(char character)
+{
+    return character == '^' || character == '/' || character == '*' || character == '%' || character == '+' || character == '-' || character == '(' || character == ')' || character == ',';
+}
+
+ExpressionTree::ExpressionTree(char* postfix) {
+    Node** stack = new Node*[strlen(postfix)];
+    int stackTop = -1;
+    for (int i=0; i<strlen(postfix); i++) {
+        if (!isOperator(postfix[i])) {
+            Node* newNode = new Node;
+            newNode->data = postfix[i];
+            newNode->rightChild = newNode->leftChild = NULL;
+            stack[++stackTop] = newNode;
+        }
+        else {
+            Node* newNode = new Node;
+            newNode->data = postfix[i];
+            newNode->leftChild = stack[stackTop--];
+            newNode->rightChild = stack[stackTop--];
+            stack[++stackTop] = newNode;
+        }
+    }
+    this->root = stack[stackTop--];
+}
+
+void ExpressionTree::inOrder (Node* node) {
+    if (node->rightChild != NULL)
+    inOrder(node->rightChild);
+
+    cout << node->data;
+
+    if (node->leftChild != NULL)
+    inOrder(node->leftChild);
+}
+
+void ExpressionTree::postOrder (Node* node) {
+    if (node->rightChild != NULL)
+    postOrder(node->rightChild);
+
+    if (node->leftChild != NULL)
+    postOrder(node->leftChild);
+
+    cout << node->data;
+}
+
+void ExpressionTree::preOrder (Node* node) {
+    cout << node->data;
+
+    if (node->rightChild != NULL)
+    preOrder(node->rightChild);
+
+    if (node->leftChild != NULL)
+    preOrder(node->leftChild);
+}
