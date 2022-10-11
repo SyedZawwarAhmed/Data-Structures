c22901bf55c3e75b2ef587c303d6d42aa269f4cb
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-10-11 15:47:44 +0500

    feat: create MultidimensionalArray.cpp

---
+++ Array/Column Major/MultidimensionalArray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "MultidimensionalArray.h"
+
+using namespace std;
+
+MultidimensionalArray::MultidimensionalArray(int noOfDimensions, int sizesOfDimensions[]) {
+    this->noOfDimensions = noOfDimensions;
+    this->sizesOfDimensions = sizesOfDimensions;
+    int totalSize = sizeof(int);
+    for (int i=0; i<noOfDimensions; i++) {
+        totalSize *= sizesOfDimensions[i];
+    }
+    this->data = (int*)(malloc(totalSize));
+}
+
+int MultidimensionalArray::getAddress(int indices[]) {
+    uintptr_t baseAddress = (uintptr_t)(this->data);
+    int slot = 0;
+    for (int i=this->noOfDimensions-1; i>=0; i--) {
+        int product = 1;
+        for (int j=i+1; j<this->noOfDimensions; j++) {
+            product *= this->sizesOfDimensions[j];
+        }
+        product *= indices[i];
+        slot += product;
+    }
+
+    int address = baseAddress + slot*sizeof(int);
+    return address;
+}
+
+int MultidimensionalArray::getItem(int indices[]) {
+    return *((int*)(this->getAddress(indices)));
+}
+
+void MultidimensionalArray::setItem(int item, int indices[]) {
+    *((int*)(this->getAddress(indices))) = item;
+}
+
