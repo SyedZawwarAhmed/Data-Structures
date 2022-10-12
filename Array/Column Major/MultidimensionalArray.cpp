3d4160b72a748c811d1d0b51ec86c428b1866dfa
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-10-12 23:22:24 +0500

    update MultidimensionalArray.cpp

--- Array/Column Major/MultidimensionalArray.cpp
+++ Array/Column Major/MultidimensionalArray.cpp
@@ -3,37 +3,48 @@
 
 using namespace std;
 
-MultidimensionalArray::MultidimensionalArray(int noOfDimensions, int sizesOfDimensions[]) {
+MultidimensionalArray::MultidimensionalArray(int noOfDimensions, int sizesOfDimensions[])
+{
     this->noOfDimensions = noOfDimensions;
     this->sizesOfDimensions = sizesOfDimensions;
     int totalSize = sizeof(int);
-    for (int i=0; i<noOfDimensions; i++) {
+    for (int i = 0; i < noOfDimensions; i++)
+    {
         totalSize *= sizesOfDimensions[i];
     }
-    this->data = (int*)(malloc(totalSize));
+    this->data = (int *)(malloc(totalSize));
 }
 
-int MultidimensionalArray::getAddress(int indices[]) {
+uintptr_t MultidimensionalArray::getAddress(int indices[])
+{
+    int *indice = new int[this->noOfDimensions];
+    for (int i = 0; i < this->noOfDimensions; i++)
+    {
+        indice[i] = indices[this->noOfDimensions - 1 - i];
+    }
     uintptr_t baseAddress = (uintptr_t)(this->data);
     int slot = 0;
-    for (int i=this->noOfDimensions-1; i>=0; i--) {
+    for (int i = 0; i < this->noOfDimensions; i++)
+    {
         int product = 1;
-        for (int j=i+1; j<this->noOfDimensions; j++) {
+        for (int j = i + 1; j <= this->noOfDimensions - 1; j++)
+        {
             product *= this->sizesOfDimensions[j];
         }
-        product *= indices[i];
+        product *= indice[i];
         slot += product;
     }
 
-    int address = baseAddress + slot*sizeof(int);
+    uintptr_t address = baseAddress + slot * sizeof(int);
     return address;
 }
 
-int MultidimensionalArray::getItem(int indices[]) {
-    return *((int*)(this->getAddress(indices)));
+int MultidimensionalArray::getItem(int indices[])
+{
+    return *((int *)(this->getAddress(indices)));
 }
 
-void MultidimensionalArray::setItem(int item, int indices[]) {
-    *((int*)(this->getAddress(indices))) = item;
+void MultidimensionalArray::setItem(int item, int indices[])
+{
+    *((int *)(this->getAddress(indices))) = item;
 }
-
