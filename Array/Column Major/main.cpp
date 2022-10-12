097081c6fc981bd57d91c92a1cccba8b686cf146
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-10-12 23:22:08 +0500

    update main.cpp

--- Array/Column Major/main.cpp
+++ Array/Column Major/main.cpp
@@ -3,30 +3,40 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int noOfDimensions = 3;
-    int sizesOfDimensions[] = {2,3,4};
-    MultidimensionalArray arr1(noOfDimensions, sizesOfDimensions); 
+    int sizesOfDimensions[] = {2, 3, 4};
+    MultidimensionalArray arr1(noOfDimensions, sizesOfDimensions);
 
     int item = 0;
-    for (int i=0; i<2; i++) {
-        for (int j=0; j<3; j++) {
-            for (int k=0; k<4; k++) {
+    for (int k = 0; k < 2; k++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
                 int indices[] = {k, j, i};
                 arr1.setItem(++item, indices);
             }
         }
     }
 
-    for (int i=0; i<2; i++) {
-        for (int j=0; j<3; j++) {
-            for (int k=0; k<4; k++) {
+    cout << "\n" << endl;
+
+    for (int k = 0; k < 2; k++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < 4; i++)
+            {
                 int indices[] = {k, j, i};
-                cout << k << " " << j << " " << i << " "; 
+                cout << k << " " << j << " " << i << " ";
                 cout << arr1.getItem(indices) << " ";
-                cout << arr1.getAddress(indices) << endl;
+                cout << arr1.getAddress(indices) << endl; 
             }
         }
     }
+    
     return 0;
 }
