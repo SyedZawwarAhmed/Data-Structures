d69dd0a32696e38f3e20b584b567e05279ef7f06
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2022-11-20 06:59:39 +0500

    Shortest Path Algorithm (Dijkstra's Algorithm) added

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -48,10 +48,114 @@ void Graph::print()
             {
                 SetConsoleTextAttribute(h, 11);
             }
-                cout << this->adjacencyMatrix[i][j] << " ";
+            cout << this->adjacencyMatrix[i][j] << " ";
         }
         SetConsoleTextAttribute(h, 7);
 
         cout << endl;
     }
+}
+
+struct Vertex
+{
+    int index;
+    bool known = false;
+    int distance = 99999;
+    int previousVertex = -1;
+    int outdegree;
+    int *adjacentVertices;
+};
+
+int *Graph::getShortestPath(int vertex)
+{
+
+    Vertex **vertices = new Vertex *[this->numberOfVertices];
+
+    for (int i = 0; i < this->numberOfVertices; i++)
+    {
+        vertices[i] = new Vertex;
+    }
+
+    for (int i = 0; i < this->numberOfVertices; i++)
+    {
+        vertices[i]->index = i;
+        int outdegree = 0;
+        for (int j = 0; j < this->numberOfVertices; j++)
+        {
+            if (this->adjacencyMatrix[i][j] != 0)
+                outdegree++;
+        }
+        vertices[i]->outdegree = outdegree;
+
+        vertices[i]->adjacentVertices = new int[vertices[i]->outdegree];
+        int adjacentVerticesCount = 0;
+        for (int j = 0; j < this->numberOfVertices; j++)
+        {
+            if (this->adjacencyMatrix[i][j] > 0)
+            {
+                vertices[i]->adjacentVertices[adjacentVerticesCount++] = j;
+            }
+        }
+    }
+
+    for (int i = 0; i < numberOfVertices; i++)
+    {
+        if (vertices[i]->outdegree > 0)
+        {
+            vertices[i]->distance = 0;
+            break;
+        }
+    }
+
+    for (int i = 0; i < this->numberOfVertices; i++)
+    {
+        int minimumDistance = 99999;
+        int vertexSelected;
+        for (int j = 0; j < this->numberOfVertices; j++)
+        {
+            if (vertices[j]->distance < minimumDistance && vertices[j]->known == false)
+            {
+                vertices[j]->known = true;
+                minimumDistance = vertices[j]->distance;
+                vertexSelected = j;
+                break;
+            }
+        }
+        for (int j = 0; j < vertices[vertexSelected]->outdegree; j++)
+        {
+            int selectedVertexDistance = vertices[vertexSelected]->distance;
+            int weight = adjacencyMatrix[vertexSelected][vertices[vertexSelected]->adjacentVertices[j]];
+            int currentDistance = vertices[vertices[vertexSelected]->adjacentVertices[j]]->distance;
+            if (currentDistance > selectedVertexDistance + weight)
+            {
+                vertices[vertices[vertexSelected]->adjacentVertices[j]]->distance = selectedVertexDistance + weight;
+                vertices[vertices[vertexSelected]->adjacentVertices[j]]->previousVertex = vertexSelected;
+            }
+        }
+    }
+
+    int previousVerticesCount = 0;
+    int previousVertex = vertices[vertex]->previousVertex;
+    while (previousVertex > -1)
+    {
+        previousVertex = vertices[previousVertex]->previousVertex;
+        previousVerticesCount++;
+    }
+    int previousVerticesCountBackup = previousVerticesCount + 1;
+    previousVertex = vertices[vertex]->previousVertex;
+    int *shortestPath = new int[previousVerticesCountBackup];
+    shortestPath[previousVerticesCount] = vertex;
+    while (previousVertex > -1)
+    {
+        shortestPath[--previousVerticesCount] = previousVertex;
+        previousVertex = vertices[previousVertex]->previousVertex;
+    }
+
+    cout << "The Shortest Path from Source is:- ";
+    for (int i = 0; i < previousVerticesCountBackup; i++)
+    {
+        cout << shortestPath[i] << " ";
+    }
+    cout << endl;
+    return shortestPath;
 }
